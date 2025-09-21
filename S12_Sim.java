05c7d92baa8364662494eaccd467841d97856b55
Spencer Ford <spence@Spencers-MacBook-Air.local>
2025-09-21 14:59:07 -0600

    Added more files. Started implementation - initMem and writeMem() are where I started.

---
+++ S12_Sim.java
@@ -0,0 +1,13 @@
+
+public static void main(String[] args) {
+
+    //Args testing
+    if(args.length < 1) {
+        System.out.err("java S12_Sim <memFile> <optional: -o outputFileBasedName> <optional: -c cyclesToExecute>");
+    }
+
+
+
+    S12_IL sim = new S12_IL();    
+
+}
